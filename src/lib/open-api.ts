28cfc7f676bc78e18b34311c7449502c5c1206e3
vsalfafara <alfafara.vm@gmail.com>
2025-08-12 00:33:49 +0800

    Changed documentation title

--- src/lib/open-api.ts
+++ src/lib/open-api.ts
@@ -7,7 +7,7 @@ export default function configureOpenApi(app: AppOpenAPI) {
     openapi: "3.0.0",
     info: {
       version: packageJSON.version,
-      title: "Tasks API",
+      title: "Record Keeping API",
     },
   });
 
